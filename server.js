4db442d17903fc9635bb9cf5d86a1ad24c1db211
Carlos Antonino <carlosantonino.cs@gmail.com>
2024-09-15 17:22:53 -0300

    criando pagina sobre e empresa

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ app.get('/home', (req, res) => {
 app.get('/contato', (req, res) => {
     res.sendFile(path.join(__dirname, 'contato', 'index.html'));
 });
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname, 'sobre', 'index.html'));
+});
+app.get('/empresa', (req, res) => {
+    res.sendFile(path.join(__dirname, 'empresa', 'index.html'));
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
