bd25ddd7fe07a1528171bedf532211810fc23f56
Carlos Antonino <carlosantonino.cs@gmail.com>
2024-09-16 09:11:42 -0300

    criando sequelize

--- server.js
+++ server.js
@@ -1,35 +1,63 @@
 const express = require('express');
 const path = require('path');
+const Sequelize = require('sequelize');
 
 const app = express();
 
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const sequelize = new Sequelize('loja', 'carlos','reactnode',{
     host: 'localhost',
-    user: 'carlos',
-    password: 'reactnode',
-    database: 'loja'
+    dialect: 'mysql'
 });
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
+sequelize.authenticate().then(function(){
+    console.log('Conexão realizada com sucesso');
+}).catch(function(err){
+    console.log('Erro ao realizar a conexão' + err);
+})
+
+const Tarefa = sequelize.define('tarefa',{
+    Descricao: {
+        type: Sequelize.STRING,
+    },
+    status: {
+        type: Sequelize.BOOLEAN
     }
+})
 
-    console.log('connected as id ' + connection.threadId);
-});
+Tarefa.create({
+    Descricao: "Continuar Projeto Laravel",
+    status: 1
+})
+
+// Tarefa.sync({force: true});
+
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'carlos',
+//     password: 'reactnode',
+//     database: 'loja'
+// });
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+
+//     console.log('connected as id ' + connection.threadId);
+// });
 
 // Insere produtos ao banco de dados
 
-connection.query("INSERT INTO produto(Descricao, Preco, Quant) VALUES ('Caneta', 3.00 , 1)"),function(err, result){
-    if(!err) {
-        console.log('Usuário cadastrado com sucesso!');
-    } else {
-        console.log('Erro ao cadastrar usuário');
-    }
-};
+// connection.query("INSERT INTO produto(Descricao, Preco, Quant) VALUES ('Caneta', 3.00 , 1)"),function(err, result){
+//     if(!err) {
+//         console.log('Usuário cadastrado com sucesso!');
+//     } else {
+//         console.log('Erro ao cadastrar usuário');
+//     }
+// };
 
 app.use(express.static('src/pages'));
 
